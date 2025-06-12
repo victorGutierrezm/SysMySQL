ddeabf21bc4d1b39857cdc049cb5943330625fe1
victor gutierrez <hola@victorgutierrez.com>
2025-06-12 08:02:20 -0600

    new cambios x

--- src/AccesoADatos/Conexion.java
+++ src/AccesoADatos/Conexion.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:sqlserver://BD_System.mssql.somee.com:1433;databaseName=BD_System;encrypt=true;trustServerCertificate=true";
+    private static final String URl = "jdbc:sqlserver://BD_System.mssql.somee.com:1433;databaseName=BD_System;encrypt=true;trustServerCertificate=true";
     private static final String USER = "dev_sistema_SQLLogin_1";
     private static final String PASSWORD = "ytps89h3ac";
-
+//hols mundo
     public static Connection getConexion() {
         try {
             // Incluye usuario y contraseña directamente en la URL
-            return DriverManager.getConnection(URL + ";user=" + USER + ";password=" + PASSWORD);
+            return DriverManager.getConnection(URl + ";user=" + USER + ";password=" + PASSWORD);
         } catch (SQLException e) {
             System.out.println("❌ Error de conexión}: " + e.getMessage());
             return null;
